2f65b0e5669c7685e05d2eaf90002920b4182fe6
MacaronFR <denisft77@gmail.com>
2025-12-04 16:01:47 +0100

    Fix build

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import Chatbot from "./Chatbot.tsx";
 
-document.getElementById("chatbot_root").onclick = (e) => {
+document.getElementById("chatbot_root")!.onclick = (e) => {
 	e.stopPropagation()
 }
-document.getElementById("chatbot-button").addEventListener("click", () => {
-	document.getElementById("chatbot_root").classList.toggle("not-visible");
+document.getElementById("chatbot-button")!.addEventListener("click", () => {
+	document.getElementById("chatbot_root")!.classList.toggle("not-visible");
 	document.querySelectorAll(".button-icon").forEach(el => {
 		el.classList.toggle("not-visible");
 	})
